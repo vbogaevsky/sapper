5bba324987336d179f845a74de41223edbf83116
vbogaevsky <gitvbogaevsky@gmail.com>
2015-03-24 01:03:08 +0300

    keep codding

--- main.rb
+++ main.rb
@@ -2,10 +2,13 @@ require "./lib/Game.rb"
 Shoes.app(title: "Sapper", width: 300, height: 300) do 
   mines_am = 10 #in future let user choose mines ammount
   game = Game.new(mines_am)
-  field = Array.new(10) do |item|
+  field_graf = Array.new(10) do |item|
     item = Array.new(10) do |cell_button|
       flow width: 30, height: 30 do
-        button "?"
+        
+
+          end 
+        end
       end 
     end
   end 
