1990d805fec2fe6d0cde33f1980b7803ccaa645e
vbogaevsky <gitvbogaevsky@gmail.com>
2015-03-23 22:37:49 +0300

    code on

--- main.rb
+++ main.rb
@@ -1,4 +1,12 @@
 require "./lib/Game.rb"
-Shoes.app(title: "Sapper", width: 300, height: 400) do 
-  
+Shoes.app(title: "Sapper", width: 300, height: 300) do 
+  mines_am = 10 #in future let user choose mines ammount
+  game = Game.new(mines_am)
+  field = Array.new(10) do |item|
+    item = Array.new(10) do |cell_button|
+      flow width: 30, height: 30 do
+        button "?"
+      end 
+    end
+  end 
 end
